b45eb64fbf05931599cfeeb46a0601b50ec17388
mlucasdev <lucasmsantos819@gmail.com>
2023-11-18 16:31:08 -0300

    test: ensure addAndReplyQuestionsSeed() not add all Questions if have already been created'

--- src/main/seeds/add-and-reply-questions-seed.test.ts
+++ src/main/seeds/add-and-reply-questions-seed.test.ts
@@ -4,11 +4,22 @@
 
 import type { PrismaClient } from '@prisma/client'
 import type { Redis } from 'ioredis'
-import type { QuestionModel } from '@/domain/models/db-models'
+import type { AlternativeModel, QuestionModel } from '@/domain/models/db-models'
 import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
 import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
+import { addAndReplyQuestionsSeed } from './add-and-reply-questions-seed'
 import RedisMock from 'ioredis-mock'
-import addAndReplyQuestionsSeed from './add-and-reply-questions-seed'
+
+const makeFakeQuestions = (): QuestionModel[] => ([{
+  id: 'any_question_id',
+  content: 'any_content'
+}])
+
+const makeFakeAlternatives = (): AlternativeModel[] => ([{
+  id: 'any_alternative_id',
+  description: 'any_description',
+  questionId: 'any_question_id'
+}])
 
 let redis: Redis
 let prisma: PrismaClient
@@ -32,7 +43,7 @@ describe('AddAndReplyQuestions Seed', () => {
   })
 
   it('Should add all Questions in DB with Prisma', async () => {
-    await addAndReplyQuestionsSeed
+    await addAndReplyQuestionsSeed()
     const questions = await prisma.question.findMany()
     const alternatives = await prisma.alternative.findMany()
     expect(questions.length).toBe(3)
@@ -40,9 +51,19 @@ describe('AddAndReplyQuestions Seed', () => {
   })
 
   it('Should reply all Questions in Cache with Redis', async () => {
-    await addAndReplyQuestionsSeed
+    await addAndReplyQuestionsSeed()
     const questionsJson = await redis.get('questions') as string
     const questions = JSON.parse(questionsJson) as QuestionModel[]
     expect(questions.length).toBe(3)
+  })
+
+  it('Should not add all Questions if have already been created', async () => {
+    await prisma.question.createMany({ data: makeFakeQuestions() })
+    await prisma.alternative.createMany({ data: makeFakeAlternatives() })
+    await addAndReplyQuestionsSeed()
+    const questions = await prisma.question.findMany()
+    const alternatives = await prisma.alternative.findMany()
+    expect(questions.length).toBe(1)
+    expect(alternatives.length).toBe(1)
   })
 })
