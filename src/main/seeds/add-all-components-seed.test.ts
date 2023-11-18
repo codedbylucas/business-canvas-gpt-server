94c12080593802ce7b29ebabb56012dd84182084
mlucasdev <lucasmsantos819@gmail.com>
2023-11-18 16:22:07 -0300

    test: ensure addAllComponentsSeed() not add all Components if have already been created

--- src/main/seeds/add-all-components-seed.test.ts
+++ src/main/seeds/add-all-components-seed.test.ts
@@ -3,8 +3,14 @@
  */
 
 import type { PrismaClient } from '@prisma/client'
+import type { ComponentModel } from '@/domain/models/db-models'
 import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
-import addAllComponentsSeed from './add-all-components-seed'
+import { addAllComponentsSeed } from './add-all-components-seed'
+
+const makeFakeComponentModels = (): ComponentModel[] => ([
+  { id: 'any_id_1', name: 'customerSegments' },
+  { id: 'any_id_2', name: 'valuePropositions' }
+])
 
 let prisma: PrismaClient
 
@@ -23,8 +29,15 @@ describe('AddAndReplyQuestions Seed', () => {
   })
 
   it('Should add all Components in DB', async () => {
-    await addAllComponentsSeed
+    await addAllComponentsSeed()
     const components = await prisma.component.findMany()
     expect(components.length).toBe(9)
+  })
+
+  it('Should not add all Components if have already been created', async () => {
+    await prisma.component.createMany({ data: makeFakeComponentModels() })
+    await addAllComponentsSeed()
+    const components = await prisma.component.findMany()
+    expect(components.length).toBe(2)
   })
 })
