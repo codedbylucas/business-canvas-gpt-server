f6a1770f3ed12e8e829c8085e3aa40bdb18b57b8
mlucasdev <lucasmsantos819@gmail.com>
2023-11-18 20:48:52 -0300

    feat: add AccessTokenBuilderUseCase Factory to AddRandomUserUseCase Factory

--- src/main/factories/usecases/user/add-random-user-usecase-factory.ts
+++ src/main/factories/usecases/user/add-random-user-usecase-factory.ts
@@ -3,11 +3,13 @@ import { AddRandomUserUseCase } from '@/interactions/usecases/user'
 import { bcryptAdapterFactory } from '@/main/factories/infra/cryptography/bcrypt-adapter-factory'
 import { userPrismaRepoFactory } from '@/main/factories/infra/db/prisma/user-prisma-repo-factory'
 import { uuidAdapterFactory } from '@/main/factories/infra/id/uuid-adapter-factory'
+import { accessTokenBuilderUseCaseFactory } from '../access/access-token-builder-usecase-factory'
 
 export const addRandomUserUseCaseFactory = (): AddRandomUser => {
   return new AddRandomUserUseCase(
     bcryptAdapterFactory(),
     uuidAdapterFactory(),
-    userPrismaRepoFactory()
+    userPrismaRepoFactory(),
+    accessTokenBuilderUseCaseFactory()
   )
 }
