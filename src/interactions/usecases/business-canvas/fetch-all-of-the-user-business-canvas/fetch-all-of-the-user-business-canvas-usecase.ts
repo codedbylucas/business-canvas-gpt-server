81b6c152f229644cb2e2f370cae34c583b43b9fa
mlucasdev <lucasmsantos819@gmail.com>
2023-11-18 23:03:33 -0300

    feat: ensure FetchAllOfTheUserBusinessCanvasUseCase call FormatDate with correct dates

--- src/interactions/usecases/business-canvas/fetch-all-of-the-user-business-canvas/fetch-all-of-the-user-business-canvas-usecase.ts
+++ src/interactions/usecases/business-canvas/fetch-all-of-the-user-business-canvas/fetch-all-of-the-user-business-canvas-usecase.ts
@@ -1,16 +1,24 @@
 import type { FetchAllOfTheUserBusinessCanvas, FetchAllOfTheUserBusinessCanvasRes } from '@/domain/contracts'
-import { NotAllBusinessCanvasError } from '@/domain/errors'
+import type { UserBusinessCanvasSummary } from '@/domain/models/output-models'
 import type { FetchAllBusinessCanvasByUserIdRepo } from '@/interactions/contracts/db'
+import { NotAllBusinessCanvasError } from '@/domain/errors'
+import { FormatDate } from '@/domain/processes/format-date/format-date'
 import { left, right } from '@/shared/either'
 
 export class FetchAllOfTheUserBusinessCanvasUseCase implements FetchAllOfTheUserBusinessCanvas {
   constructor (private readonly fetchAllBusinessCanvasByUserIdRepo: FetchAllBusinessCanvasByUserIdRepo) {}
 
   async perform (userId: string): Promise<FetchAllOfTheUserBusinessCanvasRes> {
-    const businessCavnas = await this.fetchAllBusinessCanvasByUserIdRepo.fetchByUserId(userId)
-    if (businessCavnas.length === 0) {
+    const businessCanvnas = await this.fetchAllBusinessCanvasByUserIdRepo.fetchByUserId(userId)
+    if (businessCanvnas.length === 0) {
       return left(new NotAllBusinessCanvasError())
     }
+    const canvasOfTheUser: UserBusinessCanvasSummary[] = businessCanvnas.map(
+      canvas => ({
+        id: canvas.id,
+        name: canvas.name,
+        createdAt: FormatDate.execute(canvas.createdAt)
+      }))
     return right([])
   }
 }
