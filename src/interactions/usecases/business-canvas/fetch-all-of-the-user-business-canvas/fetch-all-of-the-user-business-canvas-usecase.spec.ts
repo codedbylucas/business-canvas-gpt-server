17294706ebed8952b6c378947c80e7131a8ad2ff
mlucasdev <lucasmsantos819@gmail.com>
2023-11-18 22:27:47 -0300

    test: ensure FetchAllOfTheUserBusinessCanvasUseCase return NotAllBusinessCanvasError  if FetchAllBusinessCanvasByUserIdRepo returns empty list

--- src/interactions/usecases/business-canvas/fetch-all-of-the-user-business-canvas/fetch-all-of-the-user-business-canvas-usecase.spec.ts
+++ src/interactions/usecases/business-canvas/fetch-all-of-the-user-business-canvas/fetch-all-of-the-user-business-canvas-usecase.spec.ts
@@ -1,5 +1,6 @@
 import type { FetchAllBusinessCanvasByUserIdRepo, UserBusinessCanvasSummaryRepo } from '@/interactions/contracts/db'
 import { FetchAllOfTheUserBusinessCanvasUseCase } from './fetch-all-of-the-user-business-canvas-usecase'
+import { NotAllBusinessCanvasError } from '@/domain/errors'
 
 const makeFetchAllBusinessCanvasByUserIdRepo = (): FetchAllBusinessCanvasByUserIdRepo => {
   class FetchAllBusinessCanvasByUserIdRepoStub implements FetchAllBusinessCanvasByUserIdRepo {
@@ -27,5 +28,23 @@ describe('FetchAllOfTheUserBusinessCanvas UseCase', () => {
     const fetchByUserIdSpy = jest.spyOn(fetchAllBusinessCanvasByUserIdRepoStub, 'fetchByUserId')
     await sut.perform('any_user_id')
     expect(fetchByUserIdSpy).toHaveBeenCalledWith('any_user_id')
+  })
+
+  it('Should return NotAllBusinessCanvasError if FetchAllBusinessCanvasByUserIdRepo returns empty list', async () => {
+    const { sut, fetchAllBusinessCanvasByUserIdRepoStub } = makeSut()
+    jest.spyOn(fetchAllBusinessCanvasByUserIdRepoStub, 'fetchByUserId').mockReturnValueOnce(
+      Promise.resolve([])
+    )
+    const result = await sut.perform('any_user_id')
+    expect(result.value).toEqual(new NotAllBusinessCanvasError())
+  })
+
+  it('Should throw if FetchAllBusinessCanvasByUserIdRepo throws', async () => {
+    const { sut, fetchAllBusinessCanvasByUserIdRepoStub } = makeSut()
+    jest.spyOn(fetchAllBusinessCanvasByUserIdRepoStub, 'fetchByUserId').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const promise = sut.perform('any_user_id')
+    await expect(promise).rejects.toThrow()
   })
 })
