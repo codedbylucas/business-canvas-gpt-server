ca7716fa5b20c34cfe752a4dce795d12071fbc94
mlucasdev <lucasmsantos819@gmail.com>
2023-11-19 18:26:08 -0300

    test: ensure integration between IdTypeValidation and Validation

---
+++ src/presentation/helpers/validators/id-type/id-type-validation.spec.ts
@@ -0,0 +1,55 @@
+import type { IdValidator } from '@/presentation/contracts'
+import { MissingParamError } from '@/presentation/errors'
+import { InvalidIdError } from '@/presentation/errors/invalid-id-error'
+import { IdTypeValidation } from './id-type-validation'
+
+const makeIdValidator = (): IdValidator => {
+  class IdValidatorStub implements IdValidator {
+    isValid (id: string): boolean {
+      return true
+    }
+  }
+  return new IdValidatorStub()
+}
+
+interface SutTypes {
+  sut: IdTypeValidation
+  idValidatorStub: IdValidator
+}
+
+const makeSut = (): SutTypes => {
+  const idValidatorStub = makeIdValidator()
+  const sut = new IdTypeValidation('userId', idValidatorStub)
+  return {
+    sut,
+    idValidatorStub
+  }
+}
+
+describe('IdType Validation', () => {
+  it('Should return MissingParamError if not informed value', () => {
+    const { sut } = makeSut()
+    const result = sut.validate({})
+    expect(result.value).toEqual(new MissingParamError('userId'))
+  })
+
+  it('Should call IdValidator with correct id', () => {
+    const { sut, idValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(idValidatorStub, 'isValid')
+    sut.validate({ userId: 'any_id' })
+    expect(isValidSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  it('Should return InvalidIdError if IdValidator fails', () => {
+    const { sut, idValidatorStub } = makeSut()
+    jest.spyOn(idValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const result = sut.validate({ userId: 'any_id' })
+    expect(result.value).toEqual(new InvalidIdError('any_id'))
+  })
+
+  it('Should return null if IdValidator is a success', () => {
+    const { sut } = makeSut()
+    const result = sut.validate({ userId: 'any_id' })
+    expect(result.value).toEqual(null)
+  })
+})
