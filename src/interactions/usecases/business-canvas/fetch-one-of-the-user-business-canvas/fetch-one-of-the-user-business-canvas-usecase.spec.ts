e25d1a408c05a382499d3f5d2fe37143f0e65af8
mlucasdev <lucasmsantos819@gmail.com>
2023-11-19 16:57:58 -0300

    test: ensure integration between FetchOneOfTheUserBusinessCanvasUseCase  and FetchOneOfTheUserBusinessCanvasRepo

---
+++ src/interactions/usecases/business-canvas/fetch-one-of-the-user-business-canvas/fetch-one-of-the-user-business-canvas-usecase.spec.ts
@@ -0,0 +1,75 @@
+import type { FetchOneOfTheUserBusinessCanvasDto } from '@/domain/contracts'
+import type { BusinessCanvasModel } from '@/domain/models/db-models'
+import type { FetchOneOfTheUserBusinessCanvasRepo } from '@/interactions/contracts/db'
+import { BusinessCanvasNotFoundError } from '@/domain/errors'
+import { FetchOneOfTheUserBusinessCanvasUseCase } from './fetch-one-of-the-user-business-canvas-usecase'
+
+const makeFakeFetchOneOfTheUserBusinessCanvasDto = (): FetchOneOfTheUserBusinessCanvasDto => ({
+  userId: 'any_user_id',
+  businessCanvasId: 'any_business_canvas_id'
+})
+
+const makeFakeBusinessCanvasModel = (): BusinessCanvasModel => ({
+  id: 'any_id',
+  name: 'any_business_canvas_name',
+  createdAt: new Date(),
+  userId: 'any_user_id',
+  components: {
+    customerSegments: ['any_customer_segments'],
+    valuePropositions: ['any_value_propositions'],
+    channels: ['any_channels'],
+    customerRelationships: ['any_customer_relationships'],
+    revenueStreams: ['any_revenue_streams'],
+    keyResources: ['any_key_resources'],
+    keyActivities: ['any_key_activities'],
+    keyPartnerships: ['any_key_partnerships'],
+    costStructure: ['any_cost_structure']
+  }
+})
+
+const makeFetchOneOfTheUserBusinessCanvasRepo = (): FetchOneOfTheUserBusinessCanvasRepo => {
+  class FetchOneOfTheUserBusinessCanvasRepoStub implements FetchOneOfTheUserBusinessCanvasRepo {
+    async fetchOneOfTheUser (dto: FetchOneOfTheUserBusinessCanvasDto): Promise<null | BusinessCanvasModel> {
+      return await Promise.resolve(makeFakeBusinessCanvasModel())
+    }
+  }
+  return new FetchOneOfTheUserBusinessCanvasRepoStub()
+}
+
+interface SutTypes {
+  sut: FetchOneOfTheUserBusinessCanvasUseCase
+  fetchOneOfTheUserBusinessCanvasRepoStub: FetchOneOfTheUserBusinessCanvasRepo
+}
+
+const makeSut = (): SutTypes => {
+  const fetchOneOfTheUserBusinessCanvasRepoStub = makeFetchOneOfTheUserBusinessCanvasRepo()
+  const sut = new FetchOneOfTheUserBusinessCanvasUseCase(fetchOneOfTheUserBusinessCanvasRepoStub)
+  return { sut, fetchOneOfTheUserBusinessCanvasRepoStub }
+}
+
+describe('FetchOneOfTheUserBusinessCanvas UseCase', () => {
+  it('Should call FetchOneOfTheUserBusinessCanvasRepo with correct user id', async () => {
+    const { sut, fetchOneOfTheUserBusinessCanvasRepoStub } = makeSut()
+    const fetchAllByUserIdSpy = jest.spyOn(fetchOneOfTheUserBusinessCanvasRepoStub, 'fetchOneOfTheUser')
+    await sut.perform(makeFakeFetchOneOfTheUserBusinessCanvasDto())
+    expect(fetchAllByUserIdSpy).toHaveBeenCalledWith(makeFakeFetchOneOfTheUserBusinessCanvasDto())
+  })
+
+  it('Should return NotAllBusinessCanvasError if FetchOneOfTheUserBusinessCanvasRepo returns empty list', async () => {
+    const { sut, fetchOneOfTheUserBusinessCanvasRepoStub } = makeSut()
+    jest.spyOn(fetchOneOfTheUserBusinessCanvasRepoStub, 'fetchOneOfTheUser').mockReturnValueOnce(
+      Promise.resolve(null)
+    )
+    const result = await sut.perform(makeFakeFetchOneOfTheUserBusinessCanvasDto())
+    expect(result.value).toEqual(new BusinessCanvasNotFoundError('any_business_canvas_id'))
+  })
+
+  it('Should throw if FetchOneOfTheUserBusinessCanvasRepo throws', async () => {
+    const { sut, fetchOneOfTheUserBusinessCanvasRepoStub } = makeSut()
+    jest.spyOn(fetchOneOfTheUserBusinessCanvasRepoStub, 'fetchOneOfTheUser').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const promise = sut.perform(makeFakeFetchOneOfTheUserBusinessCanvasDto())
+    await expect(promise).rejects.toThrow()
+  })
+})
