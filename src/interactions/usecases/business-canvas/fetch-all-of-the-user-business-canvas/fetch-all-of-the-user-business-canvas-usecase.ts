ec5e5833443f1e167d1add7f8b346a7fa2f17033
mlucasdev <lucasmsantos819@gmail.com>
2023-11-18 22:27:42 -0300

    feat: ensure FetchAllOfTheUserBusinessCanvasUseCase return NotAllBusinessCanvasError  if FetchAllBusinessCanvasByUserIdRepo returns empty list

--- src/interactions/usecases/business-canvas/fetch-all-of-the-user-business-canvas/fetch-all-of-the-user-business-canvas-usecase.ts
+++ src/interactions/usecases/business-canvas/fetch-all-of-the-user-business-canvas/fetch-all-of-the-user-business-canvas-usecase.ts
@@ -1,12 +1,16 @@
 import type { FetchAllOfTheUserBusinessCanvas, FetchAllOfTheUserBusinessCanvasRes } from '@/domain/contracts'
+import { NotAllBusinessCanvasError } from '@/domain/errors'
 import type { FetchAllBusinessCanvasByUserIdRepo } from '@/interactions/contracts/db'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class FetchAllOfTheUserBusinessCanvasUseCase implements FetchAllOfTheUserBusinessCanvas {
   constructor (private readonly fetchAllBusinessCanvasByUserIdRepo: FetchAllBusinessCanvasByUserIdRepo) {}
 
   async perform (userId: string): Promise<FetchAllOfTheUserBusinessCanvasRes> {
-    await this.fetchAllBusinessCanvasByUserIdRepo.fetchByUserId(userId)
+    const businessCavnas = await this.fetchAllBusinessCanvasByUserIdRepo.fetchByUserId(userId)
+    if (businessCavnas.length === 0) {
+      return left(new NotAllBusinessCanvasError())
+    }
     return right([])
   }
 }
