906a23f527530895730ecaf814376deaa1daf301
mlucasdev <lucasmsantos819@gmail.com>
2023-11-19 18:00:01 -0300

    feat: ensure BusinessCanvasPrismaRepo return an BusinessCanvasModel

--- src/infra/db/prisma/business-canvas/business-canvas-repo.ts
+++ src/infra/db/prisma/business-canvas/business-canvas-repo.ts
@@ -1,8 +1,9 @@
+import type { AddBusinessCanvasRepo, FetchAllBusinessCanvasByUserIdRepo, FetchOneOfTheUserBusinessCanvasRepo, UserBusinessCanvasSummaryRepo } from '@/interactions/contracts/db'
+import type { FetchOneOfTheUserBusinessCanvasDto } from '@/domain/contracts'
 import type { BusinessCanvasModel } from '@/domain/models/db-models'
-import type { AddBusinessCanvasRepo, FetchAllBusinessCanvasByUserIdRepo, UserBusinessCanvasSummaryRepo } from '@/interactions/contracts/db'
 import { PrismaHelper } from '../helpers/prisma-helper'
 
-export class BusinessCanvasPrismaRepo implements AddBusinessCanvasRepo, FetchAllBusinessCanvasByUserIdRepo {
+export class BusinessCanvasPrismaRepo implements AddBusinessCanvasRepo, FetchAllBusinessCanvasByUserIdRepo, FetchOneOfTheUserBusinessCanvasRepo {
   async add (dto: BusinessCanvasModel): Promise<void> {
     const { components, createdAt, id, name, userId } = dto
     const prisma = await PrismaHelper.getCli()
@@ -21,10 +22,28 @@ export class BusinessCanvasPrismaRepo implements AddBusinessCanvasRepo, FetchAll
 
   async fetchAllByUserId (userId: string): Promise<UserBusinessCanvasSummaryRepo[]> {
     const prisma = await PrismaHelper.getCli()
-    const businessCanvas = await prisma.businessCanvas.findMany({
+    const businessCanvasOrEmpty = await prisma.businessCanvas.findMany({
       where: { userId },
       select: { id: true, createdAt: true, name: true }
     })
-    return businessCanvas
+    return businessCanvasOrEmpty
+  }
+
+  async fetchOneOfTheUser (dto: FetchOneOfTheUserBusinessCanvasDto): Promise<null | BusinessCanvasModel> {
+    const prisma = await PrismaHelper.getCli()
+    const businessCanvasOrNull = await prisma.businessCanvas.findUnique({
+      where: { id: dto.businessCanvasId, userId: dto.userId },
+      include: { BusinessCanvasComponent: true }
+    })
+    if (businessCanvasOrNull) {
+      const components: Record<string, string[]> = {}
+      for (const component of businessCanvasOrNull.BusinessCanvasComponent) {
+        components[component.componentName] = component.topics
+      }
+      const { id, name, userId, createdAt } = businessCanvasOrNull
+      const businessCanvas = { id, name, userId, createdAt, components }
+      return businessCanvas as BusinessCanvasModel
+    }
+    return businessCanvasOrNull
   }
 }
