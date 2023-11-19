eabfb68150827c5d6884eb7fa54ee8f9a7462b9a
mlucasdev <lucasmsantos819@gmail.com>
2023-11-19 18:33:35 -0300

    feat: ensure integration between UuidAdapter and uuid validate()

--- src/infra/id/uuid-adapter/uuid-adapter.ts
+++ src/infra/id/uuid-adapter/uuid-adapter.ts
@@ -1,10 +1,15 @@
 import type { IdModel } from '@/domain/models/output-models'
 import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import type { IdValidator } from '@/presentation/contracts'
 import * as uuid from 'uuid'
 
-export class UuidAdapter implements IdBuilder {
+export class UuidAdapter implements IdBuilder, IdValidator {
   build (): IdModel {
     const id = uuid.v4()
     return { id }
+  }
+
+  isValid (id: string): boolean {
+    return uuid.validate(id)
   }
 }
